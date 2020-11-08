d0381e0a3d15cc3c36b0a9d826de382f7dbc0d91
kazuki.okahashi <kazuki.okahashi@persol.co.jp>
2020-11-09 00:06:42 +0900

    implement main page.

--- 10.front/myCocktails-ng/src/app/app-routing.module.ts
+++ 10.front/myCocktails-ng/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SearchCocktailsComponent } from './search-cocktails/search-cocktails.component';
+import { ManagementMaterialsComponent } from './management-materials/management-materials.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'searchCocktails', component: SearchCocktailsComponent },
+  { path: 'managementMaterials', component: ManagementMaterialsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ 10.front/myCocktails-ng/src/app/management-materials/management-materials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-management-materials',
+  templateUrl: './management-materials.component.html',
+  styleUrls: ['./management-materials.component.css']
+})
+export class ManagementMaterialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
