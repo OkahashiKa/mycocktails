6a9de79110fe2f5473f7dbdd56f15c7d8191244a
OkahashiKa <okarians.302.dev@gmail.co.jp>
2020-12-17 23:27:13 +0900

    Added packaging process to generator.

--- 20.api/10.swagger/generator.sh
+++ 20.api/10.swagger/generator.sh
@@ -8,18 +8,22 @@ if [ $# -ne 1 ]; then
     echo "API_NAME is 10.swagger/[SWAGGER DIR]"
     exit 1
 fi
+
 API_NAME=$1
 
 rm -rf ${PWD}/${API_NAME}/aspnetcore
 
-ASP_PACKAGE_BASE=mycocktail.library.
+export VERSION=`grep version ${API_NAME}/openapi.yaml | sed -e "s/version: //" | sed -e "s/ //g"`
+ASP_PACKAGE_BASE=mycocktails.library.
 ASP_API_NAME=${API_NAME}Api
 ASP_PACKAGE_NAME=${ASP_PACKAGE_BASE}${ASP_API_NAME}
-ASP_PACKAGE_TITLE=${ASP_API_NAME}
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:${CLI_VERSION} generate \
 -i /local/${API_NAME}/openapi.yaml -g aspnetcore -o /local/${API_NAME}/aspnetcore \
---additional-properties=packageVersion=0.1.0 \
+--additional-properties=packageVersion=${VERSION} \
 --additional-properties=packageName=${ASP_PACKAGE_NAME} \
 --additional-properties=packageTitle=${API_NAME} \
 -c /local/build/aspnetcore.json
+
+dotnet pack ${PWD}/${API_NAME}/aspnetcore/src/${ASP_PACKAGE_NAME}/${ASP_PACKAGE_NAME}.csproj \
+-p:PackageVersion=${VERSION} -o ${PWD}/Package
