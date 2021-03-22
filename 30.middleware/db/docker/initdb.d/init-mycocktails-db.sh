2845bcc7a35dd39ba9ac6564e8e880a4bfef3b28
kazuki.okahashi <kazuki.okahashi@cloudsteady.jp>
2021-03-22 10:53:35 +0900

    feat: create dockerfile for db container.

--- 30.middleware/db/docker/initdb.d/init-mycocktails-db.sh
+++ 30.middleware/db/docker/initdb.d/init-mycocktails-db.sh
@@ -2,19 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" <<-EOSQL
-    CREATE USER ${DBUSER};
-    CREATE DATABASE ${DBNAME};
-    GRANT ALL PRIVILEGES ON DATABASE ${DBNAME} TO ${DBADMIN};
+    CREATE USER "${DBADMIN}";
+    CREATE DATABASE "${DBNAME}";
+    GRANT ALL PRIVILEGES ON DATABASE "${DBNAME}" TO "${DBADMIN}";
 EOSQL
 
 psql -d ${DBNAME} -f /docker-entrypoint-initdb.d/mycocktails_db.ddl
 psql -d ${DBNAME} -f /docker-entrypoint-initdb.d/mycocktails_db.dml
-psql -d ${DBNAME} -c 'ALTER DATABASE "ZasekiDB" SET timezone TO 'JAPAN';'
-
-
-
-psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
-    CREATE USER test_user;
-    CREATE DATABASE test_db;
-    GRANT ALL PRIVILEGES ON DATABASE test_db TO test_user;
-EOSQL
+psql -d ${DBNAME} -c 'ALTER DATABASE "mycocktails_db" SET timezone TO 'JAPAN';'
