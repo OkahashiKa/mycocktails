2e24b4b2e62c3fd0291cd18c4b533b1a500e25f0
kazuki.okahashi <okarians.302.dev@gmail.com>
2021-09-25 17:06:42 +0900

    feat: update ddl with fix table design.

--- 30.middleware/db/sql/mycocktails-db_ddl.sql
+++ 30.middleware/db/sql/mycocktails-db_ddl.sql
@@ -38,40 +38,70 @@ CREATE TABLE t_cocktail_material (
 
 -- PostgresSQL
 CREATE TABLE m_material_category (
-    id INTEGER NOT NULL,
-    name VARCHAR(100),
-    create_at TIMESTAMP,
-    update_at TIMESTAMP,
+    id SERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE m_material (
-    id INTEGER NOT NULL,
-    name VARCHAR(100),
+    id SERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
     category_id INTEGER NOT NULL,
-    create_at TIMESTAMP,
-    update_at TIMESTAMP,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(category_id) REFERENCES m_material_category(id)
 );
 
 CREATE TABLE m_cocktail (
-    id INTEGER NOT NULL,
-    name VARCHAR(100),
+    id SERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
     remarks VARCHAR(1000),
     image BYTEA,
-    create_at TIMESTAMP,
-    update_at TIMESTAMP,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE t_cocktail_material (
+CREATE TABLE m_cocktail_recipi (
     cocktail_id INTEGER NOT NULL,
     material_id INTEGER NOT NULL,
-    quantity INTEGER,
-    create_at TIMESTAMP,
-    update_at TIMESTAMP,
+    quantity INTEGER NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
     PRIMARY KEY(cocktail_id, material_id),
     FOREIGN KEY(cocktail_id) REFERENCES m_cocktail(id),
     FOREIGN KEY(material_id) REFERENCES m_material(id)
+);
+
+CREATE TABLE m_role (
+    id SERIAL NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE m_user (
+    id VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    mail VARCHAR(100),
+    role_id INTEGER NOT NULL,
+    favo_cocktail_id INTEGER,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES m_role(id),
+    FOREIGN KEY(favo_cocktail_id) REFERENCES m_cocktail(id)
+);
+
+CREATE TABLE user_material (
+    user_id VARCHAR(100) NOT NULL,
+    material_id INTEGER NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    update_at TIMESTAMP NOT NULL,
+    delete_at TIMESTAMP,
+    PRIMARY KEY(user_id, material_id)
 );
